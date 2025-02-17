ba696003cf25345c41f58c9fd6bcc12d115fc5ca
Nikita Milko <n.milko@newlinetechnologies>
2025-02-17 15:28:13 +0200

    WIP SwiftUI Tutorials - Handling user input (Filter the list view)

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = true
+    
+    var filteredLandmarks: [Landmark] {
+        return landmarks.filter { !showFavoritesOnly || $0.isFavorite }
+    }
+    
     var body: some View {
         NavigationSplitView {
-            List(landmarks) { landmark in
+            List(filteredLandmarks) { landmark in
                 NavigationLink {
                     LandmarkDetail(landmark: landmark)
                 } label: {
